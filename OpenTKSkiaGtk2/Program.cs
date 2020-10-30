626f9f1d8ac43d9848da948f1226dc508b350c51
Andoni Morales Alastruey <ylatuya@gmail.com>
2020-10-30 18:13:30 +0100

    Initial commit

---
+++ OpenTKSkiaGtk2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Gdk;
+using Gtk;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using SkiaSharp;
+
+namespace OpenTKSkia
+{
+    class MainClass
+    {
+        private static GRContext grContext;
+        private static GRBackendRenderTarget renderTarget;
+        private const SKColorType colorType = SKColorType.Rgba8888;
+        private const GRSurfaceOrigin surfaceOrigin = GRSurfaceOrigin.BottomLeft;
+        private static SKSurface surface;
+
+        public static void Main(string[] args)
+        {
+            Toolkit.Init();
+            Application.Init();
+            var mode = GraphicsMode.Default;
+            MainWindow win = new MainWindow();
+            var glWidget = new SkiaGLWidget();
+            glWidget.RenderFrame += RenderFrame;
+            win.Add(glWidget);
+            win.ShowAll();
+            Application.Run();
+        }
+
+        private static void RenderFrame(object sender, SKSurface surface)
+        {
+            SkiaGLWidget gLWidget = sender as SkiaGLWidget;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            SKPoint center = new SKPoint(gLWidget.Allocation.Width / 2, gLWidget.Allocation.Height / 2);
+            float radius = Math.Min(gLWidget.Allocation.Width, gLWidget.Allocation.Height) / 4;
+
+
+            SKPath path = new SKPath
+            {
+                FillType = SKPathFillType.EvenOdd
+            };
+
+            path.AddCircle(center.X - radius / 2, center.Y - radius / 2, radius);
+            path.AddCircle(center.X - radius / 2, center.Y + radius / 2, radius);
+            path.AddCircle(center.X + radius / 2, center.Y - radius / 2, radius);
+            path.AddCircle(center.X + radius / 2, center.Y + radius / 2, radius);
+
+            SKPaint paint = new SKPaint()
+            {
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.Cyan
+            };
+
+            canvas.DrawPath(path, paint);
+
+            paint.Style = SKPaintStyle.Stroke;
+            paint.StrokeWidth = 10;
+            paint.Color = SKColors.Magenta;
+
+            canvas.DrawPath(path, paint);
+
+        }
+    }
+}
---
+++ OpenTKSkiaGtk2/SkiaGLWidget.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using SkiaSharp;
+
+namespace OpenTKSkia
+{
+    public class SkiaGLWidget : GLWidget
+    {
+        public new event EventHandler<SKSurface> RenderFrame;
+
+        const SKColorType COLOR_TYPE = SKColorType.Rgba8888;
+        const GRSurfaceOrigin SURFACE_ORIGIN = GRSurfaceOrigin.BottomLeft;
+
+        GRContext grContext;
+        GRBackendRenderTarget renderTarget;
+        SKSurface surface;
+
+        public SkiaGLWidget()
+        {
+        }
+
+        protected override void OnRenderFrame()
+        {
+            // create the contexts if not done already
+            if (grContext == null)
+            {
+                var glInterface = GRGlInterface.Create();
+                grContext = GRContext.CreateGl(glInterface);
+            }
+            // manage the drawing surface
+            var alloc = Allocation;
+            var res = (int)Math.Max(1.0, Gdk.Screen.Default.Resolution / 96.0);
+            var w = Math.Max(0, alloc.Width * res);
+            var h = Math.Max(0, alloc.Height * res);
+            if (renderTarget == null || surface == null || renderTarget.Width != w || renderTarget.Height != h)
+            {
+                // create or update the dimensions
+                renderTarget?.Dispose();
+                GL.GetInteger(GetPName.FramebufferBinding, out var framebuffer);
+                GL.GetInteger(GetPName.StencilBits, out var stencil);
+                GL.GetInteger(GetPName.Samples, out var samples);
+                var maxSamples = grContext.GetMaxSurfaceSampleCount(COLOR_TYPE);
+                if (samples > maxSamples)
+                    samples = maxSamples;
+                var glInfo = new GRGlFramebufferInfo((uint)framebuffer, COLOR_TYPE.ToGlSizedFormat());
+                renderTarget = new GRBackendRenderTarget(w, h, samples, stencil, glInfo);
+                // create the surface
+                surface?.Dispose();
+                surface = SKSurface.Create(grContext, renderTarget, SURFACE_ORIGIN, COLOR_TYPE);
+            }
+            using (new SKAutoCanvasRestore(surface.Canvas, true))
+            {
+                RenderFrame?.Invoke(this, surface);
+            }
+            // update the control
+            surface.Canvas.Flush();
+            grContext.Flush();
+        }
+    }
+}
